357a80d3cd73372950fb53c210d2981c3f61cf20
İlkay Yağız Gür <gurilkayyagiz@gmail.com>
2022-12-19 18:08:37 +0300

    Initial Commit

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Ada Yagiz Gur on 19.12.2022.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id:1, imageName: "magazine-front-cover"),
+    Page(id:2, imageName: "magazine-back-cover")
+]
